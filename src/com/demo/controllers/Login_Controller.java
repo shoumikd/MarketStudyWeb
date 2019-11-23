9692aa11810b3bb709ccc6560d427dd0368f5a91
PC <PC@com>
2019-11-24 04:50:04 +0530

    MyFirstCommit

---
+++ src/com/demo/controllers/Login_Controller.java
@@ -0,0 +1,25 @@
+package com.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value="/logs")
+public class Login_Controller {
+
+	
+	/*
+	 * @RequestMapping(value="/login" , method=RequestMethod.GET) public
+	 * ModelAndView loadloginpage() { ModelAndView mav = new ModelAndView("login");
+	 * System.out.println("Login method has been called"); return mav; }
+	 */
+	
+	@RequestMapping(value="/login" , method=RequestMethod.GET)
+	public String loadloginpage()
+	{
+		return "login";
+	}
+	
+}
